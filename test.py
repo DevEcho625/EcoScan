e8d5966e31d78147e551da6a60aaa9c810d77606
DevEcho625 <anuj.m.bhatia@gmail.com>
2024-10-27 09:30:28 +0530

    Update test.py

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ add_selectbox = st.sidebar.image("EcoScan_Slogan_Image.png")
 def models():
 
 
-    mod = YOLO('best.pt')    
+    mod = YOLO('C:\Users\anujx\OneDrive\Desktop\best.pt')    
     return mod
 
 with tab1:
